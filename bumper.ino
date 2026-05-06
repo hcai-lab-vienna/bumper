39aaae051f8561cc5ef1adfaa1ca6f2ddcdf8dc4
3ernhard <bernhard-hoerl@gmx.at>
2026-05-06 11:19:48 +0200

    preprocessor selecting

--- bumper.ino
+++ bumper.ino
@@ -1,43 +1,87 @@
-#include "HX711.h"
-// #include <Adafruit_MPU6050.h>
-// #include <Adafruit_Sensor.h>
-// #include <Wire.h>
+#define USE_HX711
+// #define USE_MPU6050
 
 #define BAUD_RATE 115200
-#define DAT_PIN 2 //D2
-#define CLK_PIN 3 //D3
-#define SCALE 500.0
-#define SAMPLING_INTERVAL_CALIBRATION 100
-#define SAMPLING_INTERVAL 2
-#define TIMEOUT_MS 3
+#define TIMEOUT_MS 30
 
-HX711 loadcell;
-// Adafruit_MPU6050 mpu;
+
+#ifdef USE_HX711
+
+	#include "HX711.h"
+
+	#define DAT_PIN 2 //D2
+	#define CLK_PIN 3 //D3
+	#define SCALE 500.0
+	#define SAMPLING_INTERVAL_CALIBRATION 100
+	#define SAMPLING_INTERVAL 2
+
+	HX711 loadcell;
+
+#endif
+
+
+#ifdef USE_MPU6050
+
+	#include <Adafruit_MPU6050.h>
+	#include <Adafruit_Sensor.h>
+	#include <Wire.h>
+
+	Adafruit_MPU6050 mpu;
+
+#endif
+
 
 void setup() {
+
 	Serial.begin(BAUD_RATE);
-	loadcell.begin(DAT_PIN, CLK_PIN);
-	while (!loadcell.is_ready()) delay(100);
-	// while (!mpu.begin()) delay(100);
-	// mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
-	// mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-	// mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
-	loadcell.set_scale(SCALE);
-	loadcell.tare(SAMPLING_INTERVAL_CALIBRATION);
+
+	#ifdef USE_HX711
+		loadcell.begin(DAT_PIN, CLK_PIN);
+		while (!loadcell.is_ready()) delay(100);
+		loadcell.set_scale(SCALE);
+		loadcell.tare(SAMPLING_INTERVAL_CALIBRATION);
+	#endif
+
+	#ifdef USE_MPU6050
+		while (!mpu.begin()) delay(100);
+		mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+		mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+		mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+	#endif
+
 }
 
+
 void loop() {
-	if (loadcell.wait_ready_timeout(TIMEOUT_MS)) { 
-		long reading = loadcell.get_units(SAMPLING_INTERVAL);
-		// sensors_event_t a, g, temp;
-		// mpu.getEvent(&a, &g, &temp);
-		Serial.print(reading);//Serial.print(",");
-		// Serial.print(a.acceleration.x);Serial.print(",");
-		// Serial.print(a.acceleration.y);Serial.print(",");
-		// Serial.print(a.acceleration.z);Serial.print(",");
-		// Serial.print(g.gyro.x);Serial.print(",");
-		// Serial.print(g.gyro.y);Serial.print(",");
-		// Serial.print(g.gyro.z);//Serial.print(",");
-		Serial.println("");//Serial.println(temp.temperature);
-	}
+	#ifdef USE_HX711
+		if (loadcell.wait_ready_timeout(TIMEOUT_MS)) {
+	#else
+		delay(TIMEOUT_MS);
+	#endif
+
+		#ifdef USE_HX711
+			long reading = loadcell.get_units(SAMPLING_INTERVAL);
+			Serial.print(reading);
+			#ifdef USE_MPU6050
+				Serial.print(",");
+			#endif
+		#endif
+	
+		#ifdef USE_MPU6050
+			sensors_event_t a, g, temp;
+			mpu.getEvent(&a, &g, &temp);
+			Serial.print(a.acceleration.x);Serial.print(",");
+			Serial.print(a.acceleration.y);Serial.print(",");
+			Serial.print(a.acceleration.z);Serial.print(",");
+			Serial.print(g.gyro.x);Serial.print(",");
+			Serial.print(g.gyro.y);Serial.print(",");
+			Serial.print(g.gyro.z);//Serial.print(",");
+			//Serial.print(temp.temperature);
+		#endif
+
+		Serial.println("");
+	
+	#ifdef USE_HX711
+		}
+	#endif
 }
