4a5ee5dde0bef5a740fc6c58df19e94c9566cb51
3ernhard <bernhard-hoerl@gmx.at>
2026-06-24 12:47:04 +0200

    continuously publish to cmd_vel

--- ros_bumper_serial_reader/collision_response_node.py
+++ ros_bumper_serial_reader/collision_response_node.py
@@ -12,8 +12,10 @@ class CollisionResponseNode(Node):
 
     def __init__(self):
         super().__init__('collision_response_node')
-        self.collision_sub = self.create_subscription(Bool, '/collision_detected', self.collision_callback, 10)
-        self.cmd_vel_pub = self.create_publisher(Twist, '/cmd_vel', 10)
+        self.create_subscription(Bool, '/collision_detected', self.collision_callback, 10)
+        self.cmd_vel = self.create_publisher(Twist, '/cmd_vel', 10)
+        # current motion
+        self.linear, self.angular, self.duration = (0.5, 0.0, 0.0)
         # command_stack will be executed by the robot from top to bottom,
         # each command is a tuple of (linear speed, angular speed, duration in seconds).
         self.command_stack = []
@@ -30,37 +32,43 @@ class CollisionResponseNode(Node):
             # linear, angular, duration
             (-0.5, 0.0, 1.5),
             (0.0, a2, d2),
-            (0.5, 0.0, 0.1),
+            (0.5, 0.0, 0.0),
         ].copy()
 
-    def command_callback(self):
+    def schedule_command(self):
         "Schedules commands from command_stack and waits duration for next command (recursive)."
         self.command_timer.cancel()
         if self.command_stack:
-            linear, angular, duration = map(float, self.command_stack.pop(0))
-            self.move(linear, angular)
-            if duration:
-                self.command_timer = self.create_timer(duration, self.command_callback)
-                self.get_logger().info(f"WAIT sec:{duration:.2f}")
+            self.stop()
+            self.linear, self.angular, self.duration = map(float, self.command_stack.pop(0))
+            if self.duration:
+                self.command_timer = self.create_timer(self.duration, self.schedule_command)
 
     def collision_callback(self, msg: Bool):
+        """
+        The topic '/collision_detected' constantly publishes False / True (no collision / collision).
+        Therefore this callback is continuously executed with the frequency specified in serial_reader_node.py
+        """
         if msg.data and not self.command_stack:
-            self.stop()
             self.command_stack = self.collision_commands
         if self.command_timer.is_canceled():
-            self.command_callback()
+            self.schedule_command()
+        # Always move for real robots, simulations don't need it.
+        self.move()
 
-    def move(self, linear: float, angular: float):
+    def move(self):
+        "Sets linear and angular for the robot (self)."
         msg = Twist()
-        msg.linear.x = float(linear)
-        msg.angular.z = float(angular)
-        self.cmd_vel_pub.publish(msg)
-        self.get_logger().info(f"MOVE lin:{linear:.1f} ang:{angular:.1f}")
+        msg.linear.x = self.linear
+        msg.angular.z = self.angular
+        self.cmd_vel.publish(msg)
+        self.get_logger().info(f"MOVE {self.linear:.1f} {self.angular:.1f}")
 
     def stop(self):
+        "STOP!"
         msg = Twist()
-        self.cmd_vel_pub.publish(msg)
-        self.get_logger().info("Stopped moving.")
+        self.cmd_vel.publish(msg)
+        self.get_logger().info("STOP")
 
     def destroy_node(self):
         self.stop()
--- ros_bumper_serial_reader/serial_reader_node.py
+++ ros_bumper_serial_reader/serial_reader_node.py
@@ -4,7 +4,7 @@ import serial
 
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String, Bool
+from std_msgs.msg import Bool
 
 
 class SerialReaderNode(Node):
@@ -12,16 +12,13 @@ class SerialReaderNode(Node):
     def __init__(self):
         super().__init__('serial_reader_node')
         self.declare_parameter('port', '/dev/ttyUSB0')
-        self.declare_parameter('baudrate', 115200)
         self.declare_parameter('threshold', 100.0)
         self.port = self.get_parameter('port').get_parameter_value().string_value
-        self.baudrate = self.get_parameter('baudrate').get_parameter_value().integer_value
         self.threshold = self.get_parameter('threshold').get_parameter_value().double_value
-        self.raw_data_pub = self.create_publisher(String, 'raw_serial_data', 10)
         self.collision_pub = self.create_publisher(Bool, 'collision_detected', 10)
         try:
-            self.serial_port = serial.Serial(self.port, self.baudrate, timeout=0.1)
-            self.get_logger().info(f"Opened serial port {self.port} at {self.baudrate} baud")
+            self.serial_port = serial.Serial(self.port, baudrate=115200, timeout=0.1)
+            self.get_logger().info(f"Opened serial port {self.port}")
         except serial.SerialException as e:
             self.get_logger().error(f"Failed to open serial port: {e}")
             raise
@@ -37,17 +34,14 @@ class SerialReaderNode(Node):
             self.get_logger().error(f"Serial read error: {e}")
 
     def process_and_publish_line(self, line):
-        raw_msg = String()
-        raw_msg.data = line
-        self.raw_data_pub.publish(raw_msg)
         try:
             s1, s2 = map(float, line.split(','))
             condition = abs(s1) > self.threshold or abs(s2) > self.threshold
-            collision_msg = Bool()
-            collision_msg.data = condition
-            self.collision_pub.publish(collision_msg)
+            msg = Bool()
+            msg.data = condition
+            self.collision_pub.publish(msg)
             if condition:
-                self.get_logger().info(f"Collision detected: dms1={s1} dms2={s2} threshold={self.threshold}")
+                self.get_logger().info(f"Collision detected: {s1:7.1f} {s2:7.1f}  threshold={self.threshold}")
         except (ValueError, IndexError) as e:
             self.get_logger().warn(f"Failed to parse line: {line}. Error: {e}")
 
